8127684afe290ad2a26a0a3af22a1d0871b2b938
pegah <pegik327@gmail.com>
2022-03-11 02:06:54 +0330

    add search location

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,13 +1,13 @@
 <template>
-  <q-page class="flex column">
+  <q-page class="flex column bgClass">
     <div class="col q-pt-lg q-px-md">
-      <q-input v-model="search" placeholder="search" dark borderless>
+      <q-input @keyup.enter="getWeatherBySearch" v-model="search" placeholder="search" dark borderless>
         <template v-slot:before>
           <q-icon @click="getLocation" name="my_location" />
         </template>
         <template v-slot:hint> Field hint </template>
         <template v-slot:append>
-          <q-btn round dense flat icon="search" />
+          <q-btn round dense flat icon="search" @click="getWeatherBySearch" />
         </template>
       </q-input>
     </div>
@@ -50,10 +50,21 @@ export default {
       apiKey: "0c336cf464dd13fd0739d4090fded569",
     };
   },
+  computed: {
+    bgClass() {
+      if(this.weatherData){
+        if(this.weatherData.weather[0].icon.endsWith('n')) {
+          return 'bg-night'
+        }
+        else {
+          return 'bg-day'
+        }
+      }
+    }
+  },
   methods: {
     getLocation() {
       navigator.geolocation.getCurrentPosition((position) => {
-        console.log("position", position);
         this.lat = position.coords.latitude;
         this.lon = position.coords.longitude;
         this.getWeatherByCoords();
@@ -66,21 +77,31 @@ export default {
       this.weatherData = response.data
       });
     },
+    getWeatherBySearch() {
+      this.$axios(
+        `${this.apiUrl}?q=${this.search}&appid=${this.apiKey}&units=metric`
+      ).then((response) => {
+        this.weatherData = response.data
+      });
+    }
   },
 };
 </script>
 
-<style lang="scss">
-.q-page {
-  background: linear-gradient(to bottom, #b993d6, #8ca6db);
-}
-.degree {
-  top: -44px;
-}
-.skyline {
-  flex: 0 0 100px;
-  background: url("../assets/img/skyline.png");
-  background-size: contain;
-  background-position: center bottom;
-}
+<style lang="sass">
+.q-page
+  background: linear-gradient(to bottom, #b993d6, #8ca6db)
+  &.bg-night
+  background-image: linear-gradient(to bottom, #1330ed, #2c2eec, #3c2ceb, #4829ea, #5227e9, #5428ea, #5628ea, #5829eb, #532dee, #4d30f1, #4634f3, #3f37f6)
+  &.bg-day
+  background: linear-gradient(to bottom, #36d1dc, #5b86e5)
+.degree
+  top: -44px
+
+.skyline
+  flex: 0 0 100px
+  background: url("../assets/img/skyline.png")
+  background-size: contain
+  background-position: center bottom
+
 </style>
